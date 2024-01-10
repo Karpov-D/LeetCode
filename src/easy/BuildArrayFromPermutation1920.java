e447d22dfd443490308cf117c89dc4a49b03e864
Dmitry <karpov0910@mail.ru>
2024-01-10 05:37:11 +0300

    Решение задачи 1920

---
+++ src/easy/BuildArrayFromPermutation1920.java
@@ -0,0 +1,44 @@
+package easy;
+
+import java.util.Arrays;
+
+public class BuildArrayFromPermutation1920 {
+
+    /**
+     Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]]
+     for each 0 <= i < nums.length and return it.
+     A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+     Example 1:
+
+     Input: nums = [0,2,1,5,3,4]
+     Output: [0,1,2,4,5,3]
+     Explanation: The array ans is built as follows:
+     ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+     = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
+     = [0,1,2,4,5,3]
+     Example 2:
+
+     Input: nums = [5,0,1,2,3,4]
+     Output: [4,5,0,1,2,3]
+     Explanation: The array ans is built as follows:
+     ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
+     = [nums[5], nums[0], nums[1], nums[2], nums[3], nums[4]]
+     = [4,5,0,1,2,3]
+     */
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,2,1};
+        System.out.println(Arrays.toString(buildArrayMyDecision(nums)));
+    }
+
+    public static int[] buildArrayMyDecision(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            result[i] = nums[num];
+        }
+        return result;
+    }
+
+}
