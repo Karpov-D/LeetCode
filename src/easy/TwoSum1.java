5b073950457b78217b760af25c467f884786e288
Dmitry <karpov0910@mail.ru>
2024-01-12 06:30:59 +0300

    Решение задачи 1

---
+++ src/easy/TwoSum1.java
@@ -0,0 +1,58 @@
+package easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum1 {
+
+    /**
+     Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+     You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     You can return the answer in any order.
+
+     Example 1:
+
+     Input: nums = [2,7,11,15], target = 9
+     Output: [0,1]
+     Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+     Example 2:
+
+     Input: nums = [3,2,4], target = 6
+     Output: [1,2]
+     Example 3:
+
+     Input: nums = [3,3], target = 6
+     Output: [0,1]
+     */
+
+    public static void main(String[] args) {
+        int[] nums = new int[] {2,5,5,11};
+        System.out.println(Arrays.toString(twoSumMyDecision(nums, 10)));
+        System.out.println(Arrays.toString(twoSumBestDecision(nums, 10)));
+    }
+
+
+    public static int[] twoSumMyDecision(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int k = i + 1; k < nums.length; k++) {
+                if (nums[i] + nums[k] == target) {
+                    return new int[]{i, k};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public static int[] twoSumBestDecision(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
